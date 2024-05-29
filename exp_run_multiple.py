daface3a12b64e3b05c24c0d4ad71d3508eeec7e
Максим Демидов <myudemidov@edu.hse.ru>
2024-05-29 22:25:57 +0300

    exp for measuring best reconstruction length

--- exp_run_multiple.py
+++ exp_run_multiple.py
@@ -7,28 +7,38 @@ def run_multiple():
         '106_birds', '69_snowman', '40_block', '65_skull', '105_plush',
         '114_buddha', '55_bunny', '24_redhouse', '122_owl', '118_angel']
     idx = 0
-    errs = [0.1, 0.2, 0.5] + list(range(1, 16))
-    while idx < len(dtu_objects):
-        cur_err = errs[idx]
+    it_exps = []
+    it_upsaples = []
+    for its in range(2500, 5000, 500):
+        ups_start_point = its - 2000
+        upsample_list = list(range(ups_start_point, ups_start_point, 500))
+        it_exps.append(ups_start_point)
+        it_upsaples.append(upsample_list)
+    while idx < len(it_exps):
+        num_its = it_exps[idx]
+        upsamples = it_upsaples[idx]
         try:
             subprocess.run(
                 ["dvc", "exp", "run", "-f",
                  "--downstream", "run-eval",
-                 "--name", f"err_{cur_err:.01f}",
+                 "--name", f"its_{num_its}",
                  "-S", f"run.run_name=114_buddha",
-                 "-S", f"run.perturbs.pos={cur_err}",
-                 "-S", f"run.perturbs.dir={cur_err}",
-                 "-S", f"run.subdir_name=err_{cur_err:.01f}",
-                 "-S", f"run.run_name=114_buddha",
+                 "-S", "run.perturbs.pos=15",
+                 "-S", "run.perturbs.dir=15",
+                 "-S", f"run.subdir_name=its_{num_its}",
+                 "-S", "run.run_name=114_buddha",
                  "-S", "run.image_scale=2",
-                 "-S", "run.iterations=2500"],
+                 "-S", f"run.upsample_iterations={upsamples}",
+                 "-S", f"run.rebuild_iterations={[]}",
+                 "-S", "run.image_scale=2",
+                 "-S", f"run.iterations={num_its}"],
                 timeout=1000)
         except subprocess.TimeoutExpired:
-            print(f"Had to stop exp err_{cur_err:.01f}, rerunning")
+            print(f"Had to stop exp its_{num_its}, rerunning")
             continue
         idx += 1
         subprocess.run(
-            ["dvc", "exp", "save", "--name", f"err_{cur_err:.01f}"]
+            ["dvc", "exp", "save", "--name", f"its_{num_its}"]
         )
 
 
